feed77c98da51f2697edbfa4b5cdd631c63c0146
Festus <fosayi@myseneca.ca>
2023-10-31 21:06:53 -0400

    completed the coding first coding challenge of JavaScript refresher

--- Jonas-Javascript/complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
+++ Jonas-Javascript/complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
@@ -1,12 +1,43 @@
-'use strict'
+/** Challenge 1 
+ * Back to the two gymnastics teams, the Dolphins and the Koalas! 
+ * There is a new gymnastics discipline, which works differently.
+ * Each team competes 3 times, and then the average of the 3 scores 
+ * is calculated (so one average score per team).
+ * A team only wins if it has at least double the average score of the 
+ * other team. Otherwise, no team wins!
+ * todo 1: Create an arrow function calcAverage to calculate the average of 
+ * 3 scores. This function should have three parameters and return a 
+ * single number (the average score).
+ * todo 2: Create two new variables — scoreDolphins and scoreKoalas, 
+ * and assign the value returned from the calcAverage function to them 
+ * (you will need to call this function, and pass scores as arguments).
+ * todo 3: Create a function checkWinner that takes the average score of each
+ * team as parameters (avgDolphins and avgKoalas), and then logs the winner 
+ * to the console, together with the victory points, according to the 
+ * rule above. Example: Koalas win (30 vs. 13) 
+ * (use avgDolphins and avgKoalas instead of hard-coded values).
+ * todo 4: Use the checkWinner function to determine the winner for 
+ * both DATA 1 and DATA 2.
+ * todo 5: Ignore draws this time. Instead, log No team wins... 
+ * TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49
+ * TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
+*/
 
-function printForecast(arr){
-    let temp = "";
-    for (let i = 0; i < arr.length; i++){
-        temp += `... ${arr[i]}°C in ${i + 1} days `
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3
+
+const scoreDolphins = calcAverage(44, 23, 71)
+const scoreKoalas = calcAverage(65, 54, 49)
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= (avgKoalas * 2)) {
+        return `Dolphins win (${avgDolphins} vs. ${avgKoalas})`
     }
-    return temp;
-    
-    
+    else if (avgKoalas >= (avgDolphins * 2)) {
+        return `Koalas win (${avgKoalas} vs. ${avgDolphins})`
+    }
+    else {
+        return `No team wins`
+    }
 }
-console.log(printForecast([12, 5, -5, 0, 4]))
+
+console.log(checkWinner(scoreDolphins, scoreKoalas))
