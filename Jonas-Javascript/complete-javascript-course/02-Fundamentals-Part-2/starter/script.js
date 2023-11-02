0c87da54fc55a9c7b27dfa4ff0fa4357557561d1
Festus <fosayi@myseneca.ca>
2023-11-01 20:19:42 -0400

    JavaScript refresher of it's array and methods

--- Jonas-Javascript/complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
+++ Jonas-Javascript/complete-javascript-course/02-Fundamentals-Part-2/starter/script.js
@@ -41,3 +41,32 @@ function checkWinner(avgDolphins, avgKoalas) {
 }
 
 console.log(checkWinner(scoreDolphins, scoreKoalas))
+
+
+/** Challenge 2 
+ * Steven wants you to improve his tip calculator, using the same rules 
+ * as before — tip 15% of the bill if the bill value is between 50 and 300, 
+ * and if the value is different, the tip is 20%.
+ * todo 1: Write a function calcTip that takes any bill value as an input 
+ * and returns the corresponding tip, calculated based on the rules above 
+ * (you can check out the code from the first tip calculator challenge 
+ * if you need to). Use the function type you like the most. 
+ * Test the function using a bill value of 100.
+ * todo 2: And now let's use arrays! So, create an array called bills 
+ * containing the test data below.
+ * todo 3: Create an array called tips containing the tip value for each bill,
+ *  calculated from the function you created before.
+ * todo 4: BONUS: Create an array totals containing the total values, 
+ * so the bill + tip.
+ * TEST DATA 2: 125, 555, and 44.
+*/
+
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+}
+
+const bills = [125, 555, 44]
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[bills.length - 1])]
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[bills.length - 1] + tips[tips.length - 1]]
+console.log(total)
